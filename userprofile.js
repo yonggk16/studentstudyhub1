a98d4e5eacb2f7e6fdad4a7840c6c71db105f42a
Guo Ken Yong <ygk060616@gmail.com>
2026-03-14 10:33:02 +0800

    ui update v3

--- userprofile.js
+++ userprofile.js
@@ -2,18 +2,25 @@
 window.addEventListener("DOMContentLoaded", function() {
     // Gets data from localStorage
     const pfp = localStorage.getItem("profileImage");
-    const username = localStorage.getItem("accountUsername");
-    const email = localStorage.getItem("accountEmail");
+    const username = localStorage.getItem("username");
+    const email = localStorage.getItem("email");
     const bio = localStorage.getItem("bio");
     const about = localStorage.getItem("about");
+    const defaultPfp = "profileicon.svg";
 
     if (document.getElementById("profileImage")) {
         document.getElementById("profileImage").src = pfp || defaultPfp;
     }
 
     // Navbar profile icon
-    if (document.getElementById("navProfileIcon")) {
-        document.getElementById("navProfileIcon").src = pfp || defaultPfp;
+    const navIcon = document.getElementById("navProfileIcon");
+    if (navIcon && pfp) {
+        const img = document.createElement("img");
+        img.id = "navProfileIcon";
+        img.src = pfp;
+        img.className = "profile-icon";
+        img.style.borderRadius = "50%";
+        navIcon.parentNode.replaceChild(img, navIcon);
     }
 
     if (username && document.getElementById("displayUsername")) {
